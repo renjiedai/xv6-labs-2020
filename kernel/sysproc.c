30d85377f85f5712e10ce4c658ed8962d0a8a9d3
RenjieDai <2312617455@qq.com>
2022-07-10 19:31:58 +0800

    完成lab4-3 Alarm

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -97,3 +97,33 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64 
+sys_sigreturn(void){
+  struct proc* p=myproc();
+  //恢复现场
+  memmove(p->trapframe,&(p->resume),sizeof(struct trapframe));
+  //清零计时器
+  p->curticks=0;
+  return 0;
+}
+
+uint64 
+sys_sigalarm(void){
+  //保存ticks和handler到proc结构中
+  int ticks;
+  if(argint(0, &ticks) < 0){
+    return -1;
+  }
+  uint64 handler;
+  if(argaddr(1,&handler)<0){
+    return -1;
+  }
+  struct proc* p = myproc();
+  p->ticks=ticks;
+  p->handler=handler;
+  // 重置过去的时钟数
+  p->curticks = 0;
+
+  return 0;
+}
