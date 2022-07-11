3055a154e7d9ceadc96b55116fd65e81ca7cfbc4
RenjieDai <2312617455@qq.com>
2022-07-11 11:24:28 +0800

    完成lab5-1 Eliminate allocation from sbrk()

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -47,8 +47,14 @@ sys_sbrk(void)
   if(argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
+  // if(growproc(n) < 0)
+  //   return -1;
+   struct proc *p = myproc();
+  if (n < 0){
+    p->sz = uvmdealloc(p->pagetable, p->sz, p->sz + n);
+  }else{
+    myproc()->sz = addr + n;
+  }
   return addr;
 }
 
